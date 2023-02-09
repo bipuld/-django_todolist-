9a78089df9f63d0a13437fda42db3779539724ba
bipul dawadi <bipul>
2023-02-09 21:13:45 +0545

    list display

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from agenda.models import Task
 
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id','Task_name','status','effort')
+    list_display_links=('Task_name',)
+    list_editable=('status',)
+    list_filter=('id','Task_name','effort')
+    search_fields=('Task_name','effort')
+    
+    
+
+admin.site.register(Task,TaskAdmin)
--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Task(models.Model):
+    work_for=(
+           ('myself','myself'),   
+           ('other','other'),
+           ('pratice','pratice'), 
+             )
+    Task_name = models.CharField(max_length=300)
+    status=models.BooleanField(default=False)
+    effort=models.CharField(choices=work_for,default='myself',max_length=150)
+    Time=models.DateTimeField(auto_now_add=True,blank=True)
+    
+    def __str__(self):
+        return self.Task_name + "-" + str(self.status)
