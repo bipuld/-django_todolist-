687fbc22b18e617499e6afe067bc5a047642182a
bipul dawadi <bipul>
2023-02-12 22:41:50 +0545

    Paginator is done

--- agenda/views.py
+++ agenda/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse,redirect
 from agenda.models import Task
 from django.contrib import messages
 from agenda.forms import TaskForm
+from django.core.paginator import Paginator
 # Create your views here.
 def task(request):
     if request.method == 'POST':
@@ -12,7 +13,16 @@ def task(request):
         return redirect('task')
     else:
         task = Task.objects.all()
-        return render(request, 'todolist.html', {'task': task})
+    # this is for pagination purposes
+        paginator = Paginator(task, 5)
+        page_number = request.GET.get('page')
+        task = paginator.get_page(page_number)
+       #making page number in visible 5 range
+        context={
+            'task': task,
+            
+        }
+        return render(request, 'todolist.html',context)
 def home(request):
     context={
         
